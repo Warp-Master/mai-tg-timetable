b2f1ffce0a4875b065cff2ca63ef7505b0741f6b
Dmitrii Chernov <2003dima@gmail.com>
2024-01-25 02:38:10 +0300

    revert: disable access_log

--- middlewares.py
+++ middlewares.py
@@ -1,12 +1,9 @@
 import asyncio
-from time import time
-from os import getenv
 from typing import Any, Awaitable, Callable, Dict
 
 from aiogram.types import Update
-from config import redis_ts, redis_client
 
-ACCESS_LOG_TTL = int(getenv("ACCESS_LOG_TTL")) * 24 * 60 * 60 * 1000
+from config import redis_client
 
 
 async def statistics_middleware(
@@ -15,12 +12,6 @@ async def statistics_middleware(
         data: Dict[str, Any]
 ) -> Any:
     res = await handler(event, data)
-    ts = int(time() * 1000)
     user_id = data['event_from_user'].id
-    asyncio.ensure_future(
-        redis_ts.add('access_log', ts, user_id,
-                     labels={'handler': data['handler'].callback.__name__},
-                     retention_msecs=ACCESS_LOG_TTL)
-    )
     asyncio.ensure_future(redis_client.sadd('uniq_users', user_id))
     return res
