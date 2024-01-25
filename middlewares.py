7aa4c8bca7a9c2474cb134770fc20b81d1b48e14
Dmitrii Chernov <2003dima@gmail.com>
2024-01-26 00:48:24 +0300

    fix: prevent of collecting unfinished add_to_uniq_user tasks

--- middlewares.py
+++ middlewares.py
@@ -4,6 +4,18 @@ from typing import Any, Awaitable, Callable, Dict
 from aiogram.types import Update
 
 from config import redis_client
+import redis
+import logging
+
+
+background_tasks = set()
+
+
+async def add_to_uniq_users(user_id):
+    try:
+        await redis_client.sadd('uniq_users', user_id)
+    except redis.exceptions.ConnectionError:
+        logging.error('Cant connect to redis')
 
 
 async def statistics_middleware(
@@ -13,5 +25,7 @@ async def statistics_middleware(
 ) -> Any:
     res = await handler(event, data)
     user_id = data['event_from_user'].id
-    asyncio.ensure_future(redis_client.sadd('uniq_users', user_id))
+    task = asyncio.create_task(add_to_uniq_users(user_id))
+    background_tasks.add(task)
+    task.add_done_callback(background_tasks.discard)
     return res
